d118896b3e984ee307667127bbaf1cef995ea689
Evin-Ngoa <toneevin07@gmail.com>
2022-01-06 02:03:46 +0300

    add href to anchor tag

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -6,7 +6,7 @@ const VideoItem = ({ video }) => {
         <div className="video-item item"> 
             <img alt={video.snippet.title} className="ui image" src={video.snippet.thumbnails.medium.url} />
             <div className="content">
-                <a className="header">{video.snippet.title}</a>
+                <a className="header" href="#">{video.snippet.title}</a>
                 <div className="description">{video.snippet.description}</div>
             </div>
         </div>
