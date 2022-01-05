99b1e51e77af2f2679b26ea04b7433ac8494c4fb
Evin-Ngoa <toneevin07@gmail.com>
2022-01-06 02:01:11 +0300

    Add image alt to video item

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -4,7 +4,7 @@ import "./VideoItem.css";
 const VideoItem = ({ video }) => {
     return (
         <div className="video-item item"> 
-            <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+            <img alt={video.snippet.title} className="ui image" src={video.snippet.thumbnails.medium.url} />
             <div className="content">
                 <a className="header">{video.snippet.title}</a>
                 <div className="description">{video.snippet.description}</div>
