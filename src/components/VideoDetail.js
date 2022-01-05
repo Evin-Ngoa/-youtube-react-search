25892ec95a4ef1362c8010ab599344d82f9a5053
Evin-Ngoa <toneevin07@gmail.com>
2022-01-06 02:58:39 +0300

    Video Play in VideoDetails

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -2,17 +2,27 @@ import React from "react";
 
 const VideoDetail = ({ video }) => {
 
-    const title = video ? video.snippet.title : '';
-    const description = video ? video.snippet.description : '';
+    if(!video){
+        return <></>;
+    }
+
+    const { title, description } = video.snippet;
+    
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
 
     return (
-        <div className="ui segment">
-            <h4 className="ui header">
-                {title}
-            </h4>
-            <p>
-                {description}
-            </p>
+        <div>
+            <div className="ui embed">
+                <iframe src={videoSrc} />
+            </div>
+            <div className="ui segment">
+                <h4 className="ui header">
+                    {title}
+                </h4>
+                <p>
+                    {description}
+                </p>
+            </div>
         </div>
     );
 }
