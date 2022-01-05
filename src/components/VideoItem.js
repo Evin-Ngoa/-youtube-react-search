239fd5363c8825eec6459c2a72719b84a9af2afd
Evin-Ngoa <toneevin07@gmail.com>
2022-01-06 02:08:04 +0300

    Replace anchor with div in videoItem Component

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -6,7 +6,7 @@ const VideoItem = ({ video }) => {
         <div className="video-item item"> 
             <img alt={video.snippet.title} className="ui image" src={video.snippet.thumbnails.medium.url} />
             <div className="content">
-                <a className="header" href="#">{video.snippet.title}</a>
+                <div className="header">{video.snippet.title}</div>
                 <div className="description">{video.snippet.description}</div>
             </div>
         </div>
