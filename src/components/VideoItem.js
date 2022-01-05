294cd6563b47fccd0d47edb388bedbd8101a16af
Evin-Ngoa <toneevin07@gmail.com>
2022-01-06 01:40:34 +0300

    Remove listing and show thumbnail

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-const VideoItem = ({video}) => {
-    return <li> {video.snippet.title}</li>;
+const VideoItem = ({ video }) => {
+    return (
+        <div> 
+            <img src={video.snippet.thumbnails.medium.url} />
+            {video.snippet.title}
+        </div>
+    );
 }
 
 export default VideoItem;
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,12 +1,12 @@
 import React from "react";
 import VideoItem from "./VideoItem";
 
-const VideoList = ({videos}) => {
+const VideoList = ({ videos }) => {
     const videosLst = videos.map( video => {
         return <VideoItem key={video.id.videoId} video={video} />;
     })
 
-    return <ul>{videosLst}</ul>;
+    return <div>{videosLst}</div>;
 }
 
 export default VideoList;
