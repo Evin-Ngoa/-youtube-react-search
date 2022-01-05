96f423efeca189789d0858fcda26dc748a4b7dc4
Evin-Ngoa <toneevin07@gmail.com>
2022-01-06 01:20:20 +0300

    save the response  to video state and showing results

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,25 @@ import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 
 class App extends React.Component{
-    handleSearchTermSubmit = async (searchTerm) => {
-        console.log(searchTerm);
+    state = { videos: [] }
+
+    handleSearchTermSubmit = async searchTerm => {
+ 
         const response = await youtube.get('/search',{
             params:{
                 q: searchTerm
             }
         });
 
-        console.log(response);
-        console.log(response.data.items);
+        // update video state
+        this.setState({videos: response.data.items})
     }
 
     render(){
         return (
         <div className="ui container"> 
             <SearchBar handleSearchTermSubmit = {this.handleSearchTermSubmit} />
+            {this.state.videos.length} videos found
         </div>
         )
     }
