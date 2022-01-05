347cd9e87c32c8c6959e2990e2c59a408d3a8914
Evin-Ngoa <toneevin07@gmail.com>
2022-01-06 02:41:19 +0300

    VideoDetail Component style improvement

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -3,8 +3,18 @@ import React from "react";
 const VideoDetail = ({ video }) => {
 
     const title = video ? video.snippet.title : '';
-    
-    return <div>{title}</div>
+    const description = video ? video.snippet.description : '';
+
+    return (
+        <div className="ui segment">
+            <h4 className="ui header">
+                {title}
+            </h4>
+            <p>
+                {description}
+            </p>
+        </div>
+    );
 }
 
 export default VideoDetail;
