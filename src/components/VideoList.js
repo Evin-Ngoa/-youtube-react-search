f178bd0777ef70888f69ee5d03c097ab70192216
Evin-Ngoa <toneevin07@gmail.com>
2022-01-06 01:56:17 +0300

    Custom VideoItem.css and improved videoItem component view

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -6,7 +6,7 @@ const VideoList = ({ videos }) => {
         return <VideoItem key={video.id.videoId} video={video} />;
     })
 
-    return <div>{videosLst}</div>;
+    return <div className="ui relaxed divided list">{videosLst}</div>;
 }
 
 export default VideoList;
