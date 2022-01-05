0d306055bf818fbf637e5f947dc5504342ed7fea
Evin-Ngoa <toneevin07@gmail.com>
2022-01-06 00:18:28 +0300

    Sematic UI cdn import and seachBar controlled element

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,8 +2,26 @@ import React from "react";
 
 class SearchBar extends React.Component{
 
+    state = {searchTerm : ''};
+
+    handleInputChange = (event) => {
+        this.setState({searchTerm : event.target.value})
+    }
+
     render(){
-        return <div>SearchBar</div>
+        return (
+        <div className="search-bar ui segment">
+            <form className="ui form">
+                <div className="field">
+                    <label>Video Search</label>
+                    <input type="text" 
+                    value={this.state.searchTerm}
+                    onChange={this.handleInputChange}
+                    />
+                </div>
+            </form>
+        </div>
+        );
     }
 }
 
