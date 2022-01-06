7aa53b48239734bf178879e660da57329457c030
Evin-Ngoa <toneevin07@gmail.com>
2022-01-06 03:00:38 +0300

    Iframe title add

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -7,13 +7,13 @@ const VideoDetail = ({ video }) => {
     }
 
     const { title, description } = video.snippet;
-    
+
     const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
 
     return (
         <div>
             <div className="ui embed">
-                <iframe src={videoSrc} />
+                <iframe src={videoSrc} title={title} />
             </div>
             <div className="ui segment">
                 <h4 className="ui header">
