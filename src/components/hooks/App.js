366b18864fdcb1e86700e1018e86e5c906b0a477
Evin-Ngoa <toneevin07@gmail.com>
2022-01-16 12:41:52 +0300

    App functional component from class using hooks

---
+++ src/components/hooks/App.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import Divider from "../Divider";
+import VideoDetail from "../VideoDetail";
+import VideoList from "../VideoList";
+import SearchBar from "./SearchBar";
+
+const App = ({ handleSearchTermSubmit }) => {
+    const [videos, setVideos] = useState([]);
+    const [selectedVideo, setSelectedVideo] = useState(null);
+
+    //composerDidMount Run only once
+    useEffect( () => {
+        handleSearchTermSubmit();
+    }, []);
+
+    const handleSearchTermSubmit = async searchTerm => {
+        const response = await youtube.get('/search',{
+            params:{
+                q: searchTerm
+            }
+        });
+
+        setVideos(response.data.items);
+        setSelectedVideo(response.data.items[0]);
+    }
+
+    const handleVideoSelect = (video) => {
+        setSelectedVideo(video);
+    }
+
+    return (
+        <div className="ui container"> 
+            <SearchBar handleSearchTermSubmit = {handleSearchTermSubmit} />
+            <Divider results={videos} />
+            <div className='ui grid'>
+                <div className='ui row'>
+                    <div className='eleven wide column'>
+                        <VideoDetail video={selectedVideo} />
+                    </div>
+                    <div className='five wide column'>
+                        <VideoList
+                        videos={videos} 
+                        handleVideoSelect={handleVideoSelect}
+                        />
+                     </div>
+                </div>
+            </div>
+        </div>
+        )
+}
+
+export default App;
+
